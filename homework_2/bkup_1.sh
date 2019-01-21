18ed3ef3f406297007f2d0f34326932fb732e288
Oleg Kovasorov <okovasorov@xdsoft.ru>
2019-01-21 15:02:38 +0300

    add .gitignore

--- homework_2/bkup_1.sh
+++ homework_2/bkup_1.sh
@@ -2,3 +2,8 @@
 #
 # скрипт должен раз в день генерировать непустые файлы вида 200119.log, 210119.log и т.д.
 #
+set -x
+
+Wrk_Dir=$HOME/Dropbox/Projects/devops/homework_2/logs
+
+base64 /dev/urandom | head -c 100000 > $Wrk_Dir/$(date +%d%m%y).log
