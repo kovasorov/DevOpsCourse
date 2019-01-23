76bd7fc2f360aa5572db53a0df9f007d5a46e559
Oleg Kovasorov <okovasorov@xdsoft.ru>
2019-01-23 15:46:10 +0300

    return to base

--- homework_2/bkup_1.sh
+++ homework_2/bkup_1.sh
@@ -2,11 +2,10 @@
 #
 # скрипт должен раз в день генерировать непустые файлы вида 200119.log, 210119.log и т.д.
 #
-#
-# crontab -e
-# 30 23 * * * rsync -av –delete -e 'ssh -p 2222' /home/captain/dir1_for_test captain@148.251.233.175:/your_dir_2/
-set -x
+# этот скрипт добавляем в cron:
+#                               crontab -e
+#                                   30 23 * * * bkup_1.sh
 
-Wrk_Dir=$HOME/Dropbox/Projects/devops/homework_2/logs
+Wrk_Dir=$HOME/Projects/devops/homework_2/logs
 
 base64 /dev/urandom | head -c 100K > $Wrk_Dir/$(date +%d%m%y).log
--- homework_2/bkup_2.sh
+++ homework_2/bkup_2.sh
@@ -4,25 +4,19 @@
 # срок хранения таких файлов - 1 неделя
 #
 #
-set -x
 
-Source_Dir=$HOME/Dropbox/Projects/devops/homework_2/logs/
-Bkup_Dir=$HOME/Dropbox/Projects/devops/homework_2/bkup
+# откуда
+Source_Dir=oleg@192.168.1.3:/home/Projects/devops/homework_2/logs/
+# куда
+Bkup_Dir=$HOME/Projects/devops/homework_2/bkup
 
-
+# синхронизация
+rsync -avzu $Source_Dir $Bkup_Dir
 
 cd $Bkup_Dir
-for i in *gz; do         # Гораздо лучше, но...
-    echo "$filename" | cut -f 1 -d '.' > ../exclude.lst    # ... см. подвох №2
-    echo $exname
-done
-echo $exname
-rsync -avzu -exclude=../exclude.lst $Source_Dir $Bkup_Dir
-#find ./ ! -name "*gz" -print0 | xargs -0 -n 1 -t gzip
 
-#if [ ! -f "$file.gz" ]; then 
-#    gzip "$file"; 
-#else 
-#    echo "skipping $file"
-#fi
-#gzip -q -9 * && find . -type f -mtime +1 -print0 | xargs -0 rm -f
+# пакуем всё, кроме архивов
+find ./ ! -name "*gz" -print0 | xargs -0 -n 1 -t gzip
+
+# удаляем файлы старше 7 дней
+find ./ -type f -mtime +7 -print0 | xargs -0 rm -f
